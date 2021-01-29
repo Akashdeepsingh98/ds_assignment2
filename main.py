890cf1c3a1a09d2941411880edcadc82147059a9
Akashdeepsingh98 <akashdeepsingh98@gmail.com>
2021-01-29 22:04:49 +0530

    part 1 nearly done

--- main.py
+++ main.py
@@ -33,8 +33,23 @@ def get_data(input_fn):
 
 
 def radix_sort(l, sorttype):
+    if len(l) == 0:
+        return
     for i in range(len(l[0])):
         sorted(l, key=lambda x: x[len(l[0])-i-1],
+               reverse=True if sorttype == 'desc' else False)
+
+
+def radix_sort2(l, sorttype):
+    if len(l) == 0:
+        return
+
+    #for i in l:
+    #    print(i)
+    for i in range(len(l[0][2])):
+        #print(i, end=' ')
+        # print(len(l[0][2]))
+        sorted(l, key=lambda x: x[2][len(x[2])-i-1],
                reverse=True if sorttype == 'desc' else False)
 
 
@@ -50,22 +65,61 @@ def big_merge_sort(input_fn, output_fn, partsize, sorttype='asc'):
             if not t:
                 canread = False
                 break
+            #if len(t) < 5:
+            #    print(t)
+            #    canread = False
+            #    break
             t = t.strip().split('  ')
             lines.append(t)
 
         radix_sort(lines, sorttype)
 
-        intermedfile = open('intermed'+str(intermed_fnum)+'.txt', 'w')
-        intermed_fnum+=1
-        for i in range(len(lines)):
-            lines[i] = '  '.join([ele for ele in lines[i]])
-            lines[i] += '\n'
-        intermedfile.writelines(lines)
-        intermedfile.close()
+        if len(lines)>0:
+            intermedfile = open('intermed'+str(intermed_fnum)+'.txt', 'w')
+            intermed_fnum += 1
+            for i in range(len(lines)):
+                lines[i] = '  '.join([ele for ele in lines[i]])
+                lines[i] += '\n'
+            intermedfile.writelines(lines)
+            intermedfile.close()
     input_file.close()
 
     output_file = open(output_fn, 'w')
-    
+    fileheap = []
+    for i in range(intermed_fnum):
+        intermedfile = open('intermed'+str(i)+'.txt', 'r')
+        t = intermedfile.readline()
+        t = t.strip().split('  ')
+        fileheap.append([i, 0, t])
+        intermedfile.close()
+    #if len(fileheap[-1][2])==1:
+    #    fileheap=fileheap[:-1]
+    if sorttype == 'asc':
+        radix_sort2(fileheap, sorttype)
+        intermedsdone = 0
+        while intermedsdone < intermed_fnum:
+            top = fileheap[0]
+            line = '  '.join([ele for ele in top[2]])
+            topfile = top[0]
+            toplinenum = top[1]
+            intermedfile = open('intermed'+str(topfile)+'.txt', 'r')
+            newline = intermedfile.readline()
+            for i in range(toplinenum+1):
+                newline = intermedfile.readline()
+            if newline:
+                fileheap[0][2] = newline.strip().split('  ')
+                fileheap[0][1] = toplinenum+1
+            else:
+                intermedsdone += 1
+                fileheap = fileheap[1:]
+            radix_sort2(fileheap, sorttype)
+            output_file.write(line+'\n')
+            intermedfile.close()
+        pass
+    else:
+        pass
+
+    output_file.close()
 
 
 def main():
@@ -77,7 +131,7 @@ def main():
     user_cols = []
     for i in range(5, n_args):
         user_cols.append(sys.argv[i])
-    limit_memory(memlimit)
+    # limit_memory(memlimit)
 
     #data = get_data(input_file)
     partsize = 1000
