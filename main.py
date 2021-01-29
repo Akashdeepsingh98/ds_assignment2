455de0fcd8e459a99eda6ccdb4b1b5b889ca18f1
Akashdeepsingh98 <akashdeepsingh98@gmail.com>
2021-01-29 16:55:21 +0530

    data dictionary ready

--- main.py
+++ main.py
@@ -2,6 +2,35 @@ import resource
 import sys
 
 
+def limit_memory(memlimit):
+    soft, hard = resource.getrlimit(resource.RLIMIT_AS)
+    resource.setrlimit(resource.RLIMIT_AS, (memlimit, hard))
+
+
+def get_data(input_fn):
+    metadata = open('Metadata.txt', 'r')
+    data = {}
+    while True:
+        col = metadata.readline()
+        if not col:
+            break
+        col = col.strip().split(',')
+        data[col[0]] = []
+    datafile = open(input_fn, 'r')
+
+    while True:
+        row = datafile.readline()
+        if not row:
+            break
+        row = row.strip().split('  ')
+        j = 0
+        for col in data.keys():
+            data[col].append(row[j])
+            j += 1
+    
+    return data
+
+
 def main():
     n_args = len(sys.argv)
     input_file = sys.argv[1]
@@ -11,6 +40,10 @@ def main():
     user_cols = []
     for i in range(5, n_args):
         user_cols.append(sys.argv[i])
+    limit_memory(memlimit)
+
+    data = get_data(input_file)
+    print(data)
 
 
 if __name__ == '__main__':
